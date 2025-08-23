601a973524cc83ef2d20a07777fec0c984638c86
chaimaahainoub8 <chaimaa.hainoub1@gmail.com>
2025-08-24 01:44:13 +0200

    day 3 week 1 ninja exercices modified

--- week_1/day_3/exercises/exercises_xp_ninja.py
+++ week_1/day_3/exercises/exercises_xp_ninja.py
@@ -1,77 +1,89 @@
 # ------ Exercise 1
-
 class Phone:
     def __init__(self, phone_number):
         self.phone_number = phone_number
         self.call_history = []
         self.messages = []
 
-    def call(self, other_phone):
-        call_record = f"Call from {self.phone_number} to {other_phone.phone_number}"
+    def call(self, other_phone) :
+        call_record = f"{self.phone_number} called {other_phone.phone_number}"
         print(call_record)
         self.call_history.append(call_record)
-        other_phone.call_history.append(call_record)
 
     def show_call_history(self):
-        print(self.call_history)
+        print(f"\n----CALL-HISTORY-FOR{self.phone_number}----")
+        if not self.call_history:
+            print("----NO-CALLS-MADE----")
+        else :
+            for record in self.call_history: 
+                print(record)
 
     def send_message(self, other_phone, content):
-        message = {
-            'to': other_phone.phone_number,
-            'from': self.phone_number,
-            'content': content
+        message_data ={
+            'to' : other_phone.phone_number,
+            'from' : self.phone_number ,
+            'content' : content ,
         }
-        self.messages.append(message)
-        other_phone.messages.append(message)
+        self.messages.append(message_data)
+        other_phone.messages.append(message_data)  
+        print(f"message sent from {self.phone_number} to {other_phone.phone_number} . ")
+
+    def show_all_messages(self) :
+        print(f"\n--- All messages for {self.phone_number}----")
+        if not self.messages :
+            print("No messages .")
+        else : 
+            for msg in self.messages :
+                print(msg)
 
     def show_outgoing_messages(self):
-        outgoing = []
-        for message in self.messages:
-            if message['from'] == self.phone_number:
-                outgoing.append(message)
-        print(outgoing)
+        print(f"\n----Outgoing Messages from {self.phone_number}---")
+        outgoing = [msg for msg in self.messages if msg['from'] == self.phone_number]
+        if not outgoing:
+            print(" no outgoing messages.")
+        else:
+            for msg in outgoing :
+                print(f"To: {msg['to']} , content : {msg['content']} . ")
 
-    def show_incoming_messages(self):
-        incoming = []
-        for message in self.messages:
-            if message['to'] == self.phone_number:
-                incoming.append(message)
-        print(incoming)
+    def show_incoming_messages(self) : 
+        print(f"\n... Incoming messages to {self.phone_number}...")
+        incoming = [msg for msg in self.messages if msg['to'] == self.phone_number]
+        if not incoming :
+            print(" No incoming messages .")
+        else : 
+            for msg in incoming : 
+                print(f"From :{msg['from']} , content : {msg['content']}")
 
-    def show_messages_from(self, other_phone):
-        messages_from_contact = []
-        for message in self.messages:
-            if message['from'] == other_phone.phone_number and message['to'] == self.phone_number:
-                messages_from_contact.append(message)
-            elif message['to'] == other_phone.phone_number and message['from'] == self.phone_number:
-                messages_from_contact.append(message)
-        print(messages_from_contact)
 
-# Test your code !!!
+
 phone1 = Phone("111-222-333")
 phone2 = Phone("444-555-666")
-phone3 = Phone("777-888-999")
 
 phone1.call(phone2)
-phone2.call(phone3)
+phone1.call(phone2)
+phone2.call(phone1)
 
-print("\nPhone 1 call history:")
 phone1.show_call_history()
-print("Phone 2 call history:")
-phone2.show_call_history()
+phone2.show_call_history() 
 
-phone1.send_message(phone2, "Hello!")
-phone2.send_message(phone1, "Hi, how are you?")
-phone1.send_message(phone3, "Are you free tomorrow?")
 
-print("\nPhone 1 outgoing messages:")
+print("\n" + "="*20 + "\n")
+phone1.send_message(phone2, "Hello, how are you?")
+phone2.send_message(phone1, "I'm fine, thanks! And you?")
+phone1.send_message(phone2, "Doing great!")
+
+
+phone1.show_all_messages()
+phone2.show_all_messages()
+
+
 phone1.show_outgoing_messages()
-
-print("\nPhone 1 incoming messages:")
 phone1.show_incoming_messages()
 
-print("\nMessages between Phone 1 and Phone 2:")
-phone1.show_messages_from(phone2)
+print("-" * 10)
+phone2.show_outgoing_messages()
+phone2.show_incoming_messages()                    
+
 
 
 # ------ Exercise 2
