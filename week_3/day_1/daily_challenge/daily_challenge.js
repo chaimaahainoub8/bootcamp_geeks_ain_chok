50781cdb35cef307209d76602f611228f28d5054
chaimaahainoub8 <chaimaa.hainoub1@gmail.com>
2025-09-09 11:36:01 +0200

    daily challenge day 2 week 3

---
+++ week_3/day_1/daily_challenge/daily_challenge.js
@@ -0,0 +1,15 @@
+let stars =""   ;
+for (let i = 1; i <= 6; i++) {
+    stars += "*";
+    console.log(stars);
+}   
+
+console.log("----------");
+
+for (let i=1; i <= 6; i++) {
+    let row = "";
+    for (let j=1; j <= i; j++) {
+        row += "*";
+    }
+    console.log(row);
+}
